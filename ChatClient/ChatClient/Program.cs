2d629eb715c1c1714623a4e207d68610b24531ba
Oleksandr Sukach <OSukach@on.seo>
2020-07-28 16:12:27 +0300

    Created

---
+++ ChatClient/ChatClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatClient
+{
+    class Program
+    {
+        private static int port = 8005; 
+        private static string address = "10.5.114.83"; 
+        static void Main(string[] args)
+        {
+            try
+            {
+                var ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                
+                socket.Connect(ipPoint);
+                Console.Write("Enter text: ");
+
+                var message = Console.ReadLine();
+                var data = Encoding.Unicode.GetBytes(message);
+
+                socket.Send(data);
+
+                data = new byte[256];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; 
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("Responce: " + builder.ToString());
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+        private static int port = 8005; 
+        public static void Main(string[] args)
+        {
+            var ipPoint = new IPEndPoint(IPAddress.Parse(GetLocalIPAddress()), port);
+            var listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listenSocket.Bind(ipPoint);
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Running...");
+
+                while (true)
+                {
+                    var handler = listenSocket.Accept();
+                    var builder = new StringBuilder();
+                    var bytes = 0; 
+                    var data = new byte[256]; 
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    string message = "Done";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
